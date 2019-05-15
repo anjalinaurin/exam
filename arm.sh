4866e3fc70f229207d5dcbaaf62a2a3711238159
anjalinaurin <anjalimohanananju@gmail.com>
2019-05-15 16:40:25 +0530

    first

---
+++ arm.sh
@@ -0,0 +1,21 @@
+echo "Enter the number: "
+read c
+
+x=$c
+sum=0
+r=0
+n=0
+while [ $x -gt 0 ]
+do
+r=`expr $x % 10`
+n=`expr $r \* $r \* $r`
+sum=`expr $sum + $n`
+x=`expr $x / 10`
+done
+
+if [ $sum -eq $c ]
+then
+echo "Armstrong Number."
+else
+echo "Not an Armstrong Number."
+fi
